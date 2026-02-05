9b20bd15a2423005cd72665a31617670721aa825
Valentin <andreyn100@bk.ru>
2026-02-06 01:40:42 +0500

    FirstUpdates

---
+++ Assets/--Scripts--/PlayerMovement2D.cs
@@ -0,0 +1,37 @@
+using Unity.Mathematics;
+using UnityEngine;
+
+public class PlayerMovement2D : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    public SpriteRenderer spriteRenderer;
+    public Animator animator;
+    void Update()
+    {
+        float x =  Input.GetAxis("Horizontal");
+        if (x < 0)
+        {
+            spriteRenderer.flipX = true;
+            animator.SetFloat("Speed", Mathf.Abs(x));
+        }
+        else if (x > 0)
+        {
+            spriteRenderer.flipX = false;
+            animator.SetFloat("Speed", Mathf.Abs(x));
+        }
+
+      
+        
+        float y =  Input.GetAxis("Vertical");
+        if(y > 0)
+        {
+            animator.SetFloat("Speed", Mathf.Abs(y));
+        }
+        else if (y < 0)
+        {
+            animator.SetFloat("Speed", Mathf.Abs(y));
+        }
+        transform.position += new Vector3(x,y,0) * (speed * Time.deltaTime);
+       
+    }
+}
